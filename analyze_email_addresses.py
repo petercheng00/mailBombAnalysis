ea3c4662bff1ef1777892d1574ec360a16bc3660
Peter Cheng <petercheng00@gmail.com>
2019-09-29 23:14:57 -0700

    Discard unusable august day1 data

--- analyze_email_addresses.py
+++ analyze_email_addresses.py
@@ -7,18 +7,10 @@ from collections import Counter
 
 email_address_regex = r'[\w\.-]+@[\w\.-]+\.\w+'
 
-# started on august 12
-start_epoch_ms = datetime.datetime(2019, 8, 12, 0, 0).timestamp() * 1000
-end_epoch_ms = datetime.datetime(2019, 8, 13, 0, 0).timestamp() * 1000
-
 names = []
 domains = []
 tlds = []
-names_day1 = []
-domains_day1 = []
-tlds_day1 = []
 num_missing = 0
-num_missing_day1 = 0
 with open('data.csv', newline='') as csvfile:
     reader = csv.reader(csvfile)
     next(reader) # skip header
@@ -35,25 +27,15 @@ with open('data.csv', newline='') as csvfile:
             names.append(name)
             domains.append(domain)
             tlds.append(tld)
-            if ms > start_epoch_ms and ms < end_epoch_ms:
-                names_day1.append(name)
-                domains_day1.append(domain)
-                tlds_day1.append(tld)
         else:
             num_missing += 1
-            if ms > start_epoch_ms and ms < end_epoch_ms:
-                num_missing_day1 += 1
 
 print(f'Read {len(names)} email addresses ({num_missing} missing)')
-print(f'Read {len(names_day1)} email addresses ({num_missing_day1} missing)')
 
 
 name_counter = Counter(names)
 domain_counter = Counter(domains)
 tld_counter = Counter(tlds)
-name_counter_day1 = Counter(names_day1)
-domain_counter_day1 = Counter(domains_day1)
-tld_counter_day1 = Counter(tlds_day1)
 
 def saveplot(title, counter, num, filename):
     plt.figure()
@@ -64,8 +46,5 @@ def saveplot(title, counter, num, filename):
     plt.savefig(filename, dpi=200)
 
 saveplot('Top 10 TLDs', tld_counter, 10, 'tld.jpg')
-saveplot('Top 10 TLDs, Day 1', tld_counter_day1, 10, 'tld-day1.jpg')
 saveplot('Top 25 Domains', domain_counter, 25, 'domains.jpg')
-saveplot('Top 25 Domains, Day1', domain_counter_day1, 25, 'domains-day1.jpg')
 saveplot('Top 25 Names', name_counter, 25, 'names.jpg')
-saveplot('Top 25 Names, Day1', name_counter_day1, 25, 'names-day1.jpg')
